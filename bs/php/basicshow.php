0fb20662278b3e141fe3b4844b4cd7fab422e8ed
CMredkulaa <58243601+CMredkulaa@users.noreply.github.com>
2022-03-28 22:07:36 +0800

    Add files via upload

---
+++ bs/php/basicshow.php
@@ -0,0 +1,18 @@
+<?php
+exec("winpymysqltest2.py");
+
+require_once("connect.php");
+$stmt = $PDO->prepare("select id,srcip,dt,packet_content from test");
+$stmt->execute();
+
+$result = $stmt->fetchALL(PDO::FETCH_ASSOC);
+
+foreach($result as $rows)
+{
+	echo $rows[id];
+	echo $rows[srcip];
+	echo $rows[dt];
+	echo $rows[packet_content];
+	echo "<br>";
+}
+?>
---
+++ bs/php/connect.php
@@ -0,0 +1,7 @@
+<?php
+    try{
+        $PDO=new PDO('mysql:host=localhost;dbname=TEST',"root","root");
+    }catch(PDOException $e){
+        echo "数据库连接失败";
+    }
+?>
