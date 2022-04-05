bd527ac05762b377670eefa2d85132bcc10071aa
CMredkulaa <58243601+CMredkulaa@users.noreply.github.com>
2022-04-05 16:57:28 +0800

    Delete ubuntu directory

--- ubuntu/welcome.php
+++
@@ -1,2 +0,0 @@
-你的账号是 <?php echo $_POST["uname"]; ?><br>
-你的密码是 <?php echo $_POST["passwd"]; ?>
--- ubuntu/zwy-extra/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    try{
-        $PDO=new PDO('mysql:host=localhost;dbname=TEST',"root","root");
-    }catch(PDOException $e){
-        echo "数据库连接失败".$e->getMessage();;
-    }
-?>
